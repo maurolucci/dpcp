151bf25a5dac20e16bfce336df422c4a7f1e017d
Mauro <maurolucci.14@gmail.com>
2026-05-14 09:55:24 -0300

    Fix script

--- instances/script_gen_cfcp_dimac.sh
+++ instances/script_gen_cfcp_dimac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SRC="dimacs2dpcp.py"
+INPUT="dimac100/*"
 OUT1="cfcp/open"
 OUT2="cfcp/closed"
 
@@ -8,11 +9,13 @@ mkdir -p cfcp
 mkdir -p cfcp/open
 mkdir -p cfcp/closed
 
-for file in dimac100/*.col:
-    filename=$(basename -- "$file")
-    name="${filename%.*}"
+for file in $INPUT
+do
+    basename=$(basename -- "$file")
+    basename=${basename%.*}
+    name="${file%.*}"
     python3 $SRC $name $OUT1
     python3 $SRC $name $OUT2 --cerrada
-    echo "$name.dpcp" >> "$OUT1/instances.txt"
-    echo "$name.dpcp" >> "$OUT2/instances.txt"
-python3
+    echo "$basename.dpcp" >> "$OUT1/instances.txt"
+    echo "$basename.dpcp" >> "$OUT2/instances.txt"
+done
