be442a5e8bbef6e535216e897b5c68f2791a6206
maurolucci <maurolucci.14@gmail.com>
2026-06-30 18:06:29 -0300

    script to run experiments

---
+++ experiments/run_exp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# experiments/run_exp.sh: Run all experiments in the experiments folder
+
+declare -a experiments=("exp11" "exp12" "exp10")
+
+for exp in "${experiments[@]}"
+do
+    echo "Running experiment: $exp"
+    cd $exp
+    nohup ./${exp}.sh &
+    cd ..
+done
