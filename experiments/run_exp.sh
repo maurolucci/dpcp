26b55ea2dcb71ac44f0a990d24f0d4633cb7b2e4
maurolucci <maurolucci.14@gmail.com>
2026-07-06 19:52:10 -0300

    run exp6

--- experiments/run_exp.sh
+++ experiments/run_exp.sh
@@ -2,7 +2,7 @@
 
 # experiments/run_exp.sh: Run all experiments in the experiments folder
 
-declare -a experiments=("exp11" "exp12" "exp10" "exp7")
+declare -a experiments=("exp11" "exp12" "exp10" "exp7" "exp6")
 
 for exp in "${experiments[@]}"
 do
