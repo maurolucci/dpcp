e93f625bf361d35d9d5f62b28adcd585aed324ed
maurolucci <maurolucci.14@gmail.com>
2026-07-06 19:23:29 -0300

    skip abort for double colored vertices

--- src/col.cpp
+++ src/col.cpp
@@ -23,7 +23,7 @@ void Col::reset_coloring() {
 }
 
 void Col::set_color(VertexId v, size_t pi, size_t qj, Color k) {
-  assert(!is_colored(v));
+  if (is_colored(v)) continue;  // Vertex already colored, skip
   coloring[v] = k;
   classes[k].insert(v);
   colorP[pi] = k;
